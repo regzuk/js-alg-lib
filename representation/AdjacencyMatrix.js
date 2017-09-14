56d036974d93715fc984041cf47b67fe262658ed
Germann Kuznetsov <germann.kuznetsov@gmail.com>
2017-09-14 16:30:09 +0300

    minor fix

--- representation/AdjacencyMatrix.js
+++ representation/AdjacencyMatrix.js
@@ -11,7 +11,6 @@ function edgeListToAdjMatrix(edgeList, isDirected, isWeighted) {
     var countVertex = edgeList.reduce(((x,y)=>x=(y[0] > x) ? y[0] :
                                             (y[1] > x) ? y[1] : x),0)+1;
     var adjMatrix = new Array (countVertex);
-    // adjMatrix.forEach(x => {x = new Array(countVertex); return x;});
     for (var i = 0; i < adjMatrix.length; i++) adjMatrix[i] = new Array (countVertex);
     adjMatrix.map(x => x.fill(0));
     edgeList.forEach(x => {
