f000dda99d154e3cd5c3079dcef2a4a68bb84b96
Germann Kuznetsov <germann.kuznetsov@gmail.com>
2017-09-14 16:49:11 +0300

    add adjacency list representation

---
+++ representation/AdjacencyList.js
@@ -0,0 +1,23 @@
+//Example
+var adjList = [[1, 3],
+               [0, 2],
+               [1, 3],
+               [0, 2]];
+
+function edgeListToAdjList(edgeList, isDirected, isWeighted) {
+    isDirected = isDirected || false;
+    isWeighted = isWeighted || false;
+
+    var countVertex = edgeList.reduce(((x,y)=>x=(y[0] > x) ? y[0] :
+                            (y[1] > x) ? y[1] : x),0)+1;
+    var adjList = new Array (countVertex);
+    for (var i = 0; i < adjList.length; i++) adjList[i] = new Array ();
+    edgeList.forEach(x => {
+        adjList[x[0]].push((isWeighted) ? {to: x[1], weight: x[2]} : x[1]);
+        if (!isDirected) adjList[x[1]].push((isWeighted) ? {to: x[0], weight: x[2]} : x[0]);
+    });
+    return adjList;
+}
+
+var edgeList = [[0, 1],[0, 3],[1, 2],[2, 4]];
+var ex1 = edgeListToAdjList (edgeList);
